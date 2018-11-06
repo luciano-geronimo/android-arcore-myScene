bcc89c401ace2b1270131b453806389e6b17ccac
geronimo-lisboa <luciano.geronimo.fnord@gmail.com>
2018-11-06 12:37:50 -0200

    Basic functionality - Sceneform detecting planes, rendering the back plane texture (the camera). Now I have to learn how to interact with the detected plane. How do I change it's material, for example?

--- app/src/main/java/myarscene/geronimo/don/myarscene/MainActivity.kt
+++ app/src/main/java/myarscene/geronimo/don/myarscene/MainActivity.kt
@@ -1,12 +1,45 @@
 package myarscene.geronimo.don.myarscene
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MyARScene"//My logcat tag
+    private val MIN_GL_VERSION = 3.0//Minimal GL version for sceneform to work.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!checkSupport(this)){
+            Log.e(TAG, "DEVICE NOT SUPPORTED")
+            return
+        }
+
         setContentView(R.layout.activity_main)
+    }
+    /**
+     * Tem que ter a sdk >= 24 e tem que ter opengl 3.0 (pelo menos).
+     * */
+    private fun checkSupport(activity: Activity): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        val openGlVersion = (activity.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager)
+            .deviceConfigurationInfo.glEsVersion.toDouble()
+        if (openGlVersion < MIN_GL_VERSION){
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        return true;
     }
 }
